8dba575fbd3de03e0e47c7371c014f7b216291a0
Dibyanayan Bandyopadhyay <dibyanayan@gmail.com>
2024-07-23 23:44:02 +0530

    Update infer.py

--- pertaining/infer.py
+++ pertaining/infer.py
@@ -112,26 +112,26 @@ def construct_prompt(
 
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-# if int(args.ept)==1:
-#     print('using trained EPT model for inference')
-#     tokenizer = AutoTokenizer.from_pretrained("/cos_mount/users/dibyanayan/deepseek_full_ept")
-#     model = AutoModelForCausalLM.from_pretrained("/cos_mount/users/dibyanayan/deepseek_full_ept").to('cuda')
-# else:
-#     print('using original pretrained model for inference')
-#     tokenizer = AutoTokenizer.from_pretrained("deepseek-ai/deepseek-coder-1.3b-base")
-#     model = AutoModelForCausalLM.from_pretrained("deepseek-ai/deepseek-coder-1.3b-base").to('cuda')
-
-
-
-
 if int(args.ept)==1:
     print('using trained EPT model for inference')
-    tokenizer = AutoTokenizer.from_pretrained("/cos_mount/users/dibyanayan/starcoder_lora_ept")
-    model = AutoModelForCausalLM.from_pretrained("/cos_mount/users/dibyanayan/starcoder_lora_ept", device_map="auto", torch_dtype=torch.bfloat16)
+    tokenizer = AutoTokenizer.from_pretrained("/cos_mount/users/dibyanayan/deepseek_full_ept")
+    model = AutoModelForCausalLM.from_pretrained("/cos_mount/users/dibyanayan/deepseek_full_ept").to('cuda')
 else:
     print('using original pretrained model for inference')
-    tokenizer = AutoTokenizer.from_pretrained("/cos_mount/users/dibyanayan/starcoder2")
-    model = AutoModelForCausalLM.from_pretrained("/cos_mount/users/dibyanayan/starcoder2", device_map="auto", torch_dtype=torch.bfloat16)
+    tokenizer = AutoTokenizer.from_pretrained("deepseek-ai/deepseek-coder-1.3b-base")
+    model = AutoModelForCausalLM.from_pretrained("deepseek-ai/deepseek-coder-1.3b-base").to('cuda')
+
+
+
+
+# if int(args.ept)==1:
+#     print('using trained EPT model for inference')
+#     tokenizer = AutoTokenizer.from_pretrained("/cos_mount/users/dibyanayan/starcoder_lora_ept")
+#     model = AutoModelForCausalLM.from_pretrained("/cos_mount/users/dibyanayan/starcoder_lora_ept", device_map="auto", torch_dtype=torch.bfloat16)
+# else:
+#     print('using original pretrained model for inference')
+#     tokenizer = AutoTokenizer.from_pretrained("/cos_mount/users/dibyanayan/starcoder2")
+#     model = AutoModelForCausalLM.from_pretrained("/cos_mount/users/dibyanayan/starcoder2", device_map="auto", torch_dtype=torch.bfloat16)
 
 
 
@@ -209,7 +209,7 @@ print('No. of examples at 2k level cross file first is', cnt)
 
 
 
-for prompt_no in tqdm(range(5)):
+for prompt_no in tqdm(range(100)):
   # print(dataset['cross_file_first'][prompt_no]['token_num'])
   # print(0/0)
   if dataset['cross_file_first'][prompt_no]['level']=='2k': 
