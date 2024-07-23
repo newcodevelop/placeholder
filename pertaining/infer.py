5555ea6ad84787ec90bbd85fafc10ba267f420ea
Dibyanayan Bandyopadhyay <dibyanayan@gmail.com>
2024-07-23 20:08:07 +0530

    Update infer.py

--- pertaining/infer.py
+++ pertaining/infer.py
@@ -209,7 +209,7 @@ print('No. of examples at 2k level cross file first is', cnt)
 
 
 
-for prompt_no in tqdm(range(40)):
+for prompt_no in tqdm(range(20)):
   # print(dataset['cross_file_first'][prompt_no]['token_num'])
   # print(0/0)
   if dataset['cross_file_first'][prompt_no]['level']=='2k': 
