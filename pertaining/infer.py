b31ca06aaf9786f18a6c748816cac895e4c800ac
Dibyanayan Bandyopadhyay <dibyanayan@gmail.com>
2024-07-23 23:57:06 +0530

    Update infer.py

--- pertaining/infer.py
+++ pertaining/infer.py
@@ -115,11 +115,11 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 if int(args.ept)==1:
     print('using trained EPT model for inference')
     tokenizer = AutoTokenizer.from_pretrained("/cos_mount/users/dibyanayan/deepseek_full_ept")
-    model = AutoModelForCausalLM.from_pretrained("/cos_mount/users/dibyanayan/deepseek_full_ept").to('cuda')
+    model = AutoModelForCausalLM.from_pretrained("/cos_mount/users/dibyanayan/deepseek_full_ept", device_map="auto", torch_dtype=torch.bfloat16)
 else:
     print('using original pretrained model for inference')
     tokenizer = AutoTokenizer.from_pretrained("deepseek-ai/deepseek-coder-1.3b-base")
-    model = AutoModelForCausalLM.from_pretrained("deepseek-ai/deepseek-coder-1.3b-base").to('cuda')
+    model = AutoModelForCausalLM.from_pretrained("deepseek-ai/deepseek-coder-1.3b-base", device_map="auto", torch_dtype=torch.bfloat16)
 
 
 
